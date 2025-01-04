b5bd2ea9c4daf553a370a082e20986488171da9b
Rahadian Fujana <fuja@aleph-labs.com>
2025-01-05 01:16:30 +0700

    add product and wording multilanguage

---
+++ whatsapp-en.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.whatsapp-button').forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default link behavior
+        const productName = this.dataset.product; // Get the product name from data-product
+        const whatsappNumber = "6281212344321";
+        const whatsappMessage = `Hello! I want to know about these ${productName} further.`;
+        const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+        
+        window.open(whatsappURL, '_blank'); // Open WhatsApp link in a new tab
+    });
+});
