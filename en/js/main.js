cea0a0414d3456e648e3f2da929a835130220b34
Rahadian Fujana <fuja@aleph-labs.com>
2025-01-11 01:23:58 +0700

    add latest content EN

--- en/js/main.js
+++ en/js/main.js
@@ -27,37 +27,37 @@
 
 
     // testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        center: false,
-        dots: true,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-right"></i>',
-            '<i class="fa fa-angle-left"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:1
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
-        }
-    });
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1500,
+    //     center: false,
+    //     dots: true,
+    //     loop: true,
+    //     margin: 25,
+    //     nav : true,
+    //     navText : [
+    //         '<i class="fa fa-angle-right"></i>',
+    //         '<i class="fa fa-angle-left"></i>'
+    //     ],
+    //     responsiveClass: true,
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         576:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:1
+    //         },
+    //         992:{
+    //             items:2
+    //         },
+    //         1200:{
+    //             items:2
+    //         }
+    //     }
+    // });
 
 
     // Facts counter
